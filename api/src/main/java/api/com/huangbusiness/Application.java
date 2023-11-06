e8390b8fd995eacc526ecf94cc44b9380f86c424
agnarrarendelle <danny20001102@gmail.com>
2023-11-06 22:38:45 +0800

    fixed package path

--- api/src/main/java/api/com/huangbusiness/Application.java
+++ api/src/main/java/api/com/huangbusiness/Application.java
@@ -1,4 +1,4 @@
-package com.huangbusiness;
+package api.com.huangbusiness;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
