d0ef731ff6898d16d0d8aa7f9854b69031f2b7be
agnarrarendelle <danny20001102@gmail.com>
2023-11-10 15:13:14 +0800

    added a Result type for http responses

--- common/src/main/java/com/huangbusiness/result/Result.java
+++ common/src/main/java/com/huangbusiness/result/Result.java
@@ -1,4 +1,38 @@
 package com.huangbusiness.result;
 
-public class Result {
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+@Data
+public class Result<T> {
+    private static int SUCCESS_CODE = 1;
+    private static int FAIL_CODE = 0;
+
+    private Integer code;
+    private String msg;
+    private T data;
+
+    public static <T> Result<T> success() {
+        Result<T> result = new Result<T>();
+        result.code = SUCCESS_CODE;
+        return result;
+    }
+
+    public static <T> Result<T> success(T object) {
+        Result<T> result = success();
+        result.data = object;
+        return result;
+    }
+
+    public static <T> Result<T> error(String msg) {
+        Result<T> result = new Result<>();
+        result.msg = msg;
+        result.code = FAIL_CODE;
+        return result;
+    }
+    public static ResponseEntity<?> error(String msg, HttpStatus statusCode) {
+        Result<?> result = error(msg);
+        return new ResponseEntity<>(result, statusCode);
+    }
 }
