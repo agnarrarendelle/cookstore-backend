4c76e6cfa23708280bd273d164f9b3fbdf54dbe3
agnarrarendelle <danny20001102@gmail.com>
2023-11-12 21:07:13 +0800

    added Dto and Vo for fields related to user login/register

---
+++ common/src/main/java/com/huangbusiness/dto/UserEntryDto.java
@@ -0,0 +1,10 @@
+package com.huangbusiness.dto;
+
+import lombok.Getter;
+
+@Getter
+public class UserEntryDto {
+    private String email;
+    private String name;
+    private String password;
+}
---
+++ common/src/main/java/com/huangbusiness/vo/UserEntryVo.java
@@ -0,0 +1,12 @@
+package com.huangbusiness.vo;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class UserEntryVo {
+    private String token;
+}
