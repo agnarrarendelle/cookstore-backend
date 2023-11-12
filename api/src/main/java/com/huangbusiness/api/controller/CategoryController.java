cc32278150a4fab4e7ca137e18f01ce96528e3cc
agnarrarendelle <danny20001102@gmail.com>
2023-11-12 22:50:47 +0800

    made sure only admin can add new categories

--- api/src/main/java/com/huangbusiness/api/controller/CategoryController.java
+++ api/src/main/java/com/huangbusiness/api/controller/CategoryController.java
@@ -3,10 +3,8 @@ package com.huangbusiness.api.controller;
 import com.huangbusiness.common.result.Result;
 import com.huangbusiness.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/category")
@@ -16,6 +14,7 @@ public class CategoryController {
     CategoryService categoryService;
 
     @PostMapping("/add")
+    @PreAuthorize("hasRole('ROLE_Admin')")
     public Result<Void> addCategory(@RequestBody String name){
         categoryService.addCategory(name);
         return Result.success();
