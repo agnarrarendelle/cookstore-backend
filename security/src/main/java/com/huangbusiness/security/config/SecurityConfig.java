521b7b0ecbcb39c8c66e87276e271d59a5bde035
agnarrarendelle <danny20001102@gmail.com>
2023-11-12 21:10:54 +0800

    added initial security config

---
+++ security/src/main/java/com/huangbusiness/security/config/SecurityConfig.java
@@ -0,0 +1,64 @@
+package com.huangbusiness.security.config;
+
+import com.huangbusiness.security.filter.JwtAuthFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.ExceptionTranslationFilter;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+@Configuration
+public class SecurityConfig {
+    @Autowired
+    JwtAuthFilter jwtAuthFilter;
+
+    private static final String[] AUTH_WHITELIST = {
+            "/user/login",
+            "/user/register",
+//            "/index/**",
+//            "/product/**",
+//            "/stripe/**",
+//            "/ws-endpoint/**",
+            // -- Swagger UI v2
+            "/v2/api-docs",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui.html",
+            "/webjars/**",
+            // -- Swagger UI v3 (OpenAPI)
+            "/v3/api-docs/**",
+            "/swagger-ui/**"
+            // other public endpoints of your API may be appended to this array
+    };
+
+    @Bean
+    BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+        .csrf(AbstractHttpConfigurer::disable)
+        .cors(AbstractHttpConfigurer::disable)
+        .sessionManagement(AbstractHttpConfigurer::disable)
+           .authorizeHttpRequests(request -> {
+                request.requestMatchers(AUTH_WHITELIST).permitAll()
+                        .anyRequest().authenticated();
+            });
+        http.addFilterBefore(jwtAuthFilter, ExceptionTranslationFilter.class);
+
+        return http.build();
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception{
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+}
