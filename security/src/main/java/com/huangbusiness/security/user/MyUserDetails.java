b54d361709c5ce31fdc532173542e2e6672b4cf9
agnarrarendelle <danny20001102@gmail.com>
2023-11-12 21:10:43 +0800

    added custom UserDetails and UserDetailsService classes

---
+++ security/src/main/java/com/huangbusiness/security/user/MyUserDetails.java
@@ -0,0 +1,70 @@
+package com.huangbusiness.security.user;
+
+import com.huangbusiness.entity.RoleEntity;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+public class MyUserDetails implements UserDetails {
+
+    private final Integer userId;
+    @Getter
+    private final String email;
+    private final String name;
+    private final String password;
+
+    private final List<SimpleGrantedAuthority> authorities;
+
+    public MyUserDetails(Integer userId, String email, String name, String password, List<RoleEntity> roles) {
+        this.userId = userId;
+        this.email = email;
+        this.name = name;
+        this.password = password;
+        this.authorities = roles.stream()
+                .map(
+                        role -> new SimpleGrantedAuthority(role.toString())
+                )
+                .toList();
+
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.name;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
---
+++ security/src/main/java/com/huangbusiness/security/user/MyUserDetailsService.java
@@ -0,0 +1,20 @@
+package com.huangbusiness.security.user;
+
+import com.huangbusiness.entity.UserEntity;
+import com.huangbusiness.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+    @Autowired
+    UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        UserEntity user = userRepository.findUserByEmail(email).orElseThrow(()->new UsernameNotFoundException("Not found email: " + email));
+        return new MyUserDetails(user.getId(), user.getEmail(), user.getUsername(), user.getPassword(), user.getRoles());
+    }
+}
