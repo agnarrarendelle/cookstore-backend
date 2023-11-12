27d2c9954f5512bde9df114ba4c52795b3abf21e
agnarrarendelle <danny20001102@gmail.com>
2023-11-13 00:29:05 +0800

    added validation rules to user entry dto

--- common/src/main/java/com/huangbusiness/common/UserEntryDto.java
+++ common/src/main/java/com/huangbusiness/common/UserEntryDto.java
@@ -1,10 +1,22 @@
-package com.huangbusiness.common.dto;
+package com.huangbusiness.common;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 @Getter
 public class UserEntryDto {
+
+    public static interface UserRegisterAction {
+    }
+
+    @Email
+    @NotBlank
     private String email;
+
+    @NotBlank(groups = UserRegisterAction.class)
     private String name;
+
+    @NotBlank
     private String password;
 }
