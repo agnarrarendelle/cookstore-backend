7bc453032b73416ace366e7796e43ec9bdc0620d
agnarrarendelle <danny20001102@gmail.com>
2023-11-12 22:50:35 +0800

    fixed wrong way of configuring auth header in swappger ui

--- api/src/main/java/com/huangbusiness/api/swagger/SwaggerConfig.java
+++ api/src/main/java/com/huangbusiness/api/swagger/SwaggerConfig.java
@@ -1,31 +1,24 @@
 package com.huangbusiness.api.swagger;
 
-import io.swagger.v3.oas.annotations.enums.ParameterIn;
-import io.swagger.v3.oas.models.Operation;
-import io.swagger.v3.oas.models.media.StringSchema;
-import io.swagger.v3.oas.models.parameters.Parameter;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springdoc.core.customizers.OperationCustomizer;
-import org.springframework.http.HttpHeaders;
-import org.springframework.web.method.HandlerMethod;
+import java.util.Arrays;
 
 @Configuration
 public class SwaggerConfig {
     @Bean
-    public OperationCustomizer customGlobalHeaders() {
-
-        return (Operation operation, HandlerMethod handlerMethod) -> {
-
-            Parameter missingParam1 = new Parameter()
-                    .in(ParameterIn.HEADER.toString())
-                    .schema(new StringSchema())
-                    .name(HttpHeaders.AUTHORIZATION)
-                    .description("Jwt Token")
-                    .required(false);
-
-            operation.addParametersItem(missingParam1);
-            return operation;
-        };
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components().addSecuritySchemes("bearer-jwt",
+                        new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
+                                .in(SecurityScheme.In.HEADER).name("Authorization")))
+                .info(new Info().title("App API").version("snapshot"))
+                .addSecurityItem(
+                        new SecurityRequirement().addList("bearer-jwt", Arrays.asList("read", "write")));
     }
 }
