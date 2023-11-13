3dbd98653173b3d6eef016e52188c328c55a7307
agnarrarendelle <danny20001102@gmail.com>
2023-11-13 16:02:41 +0800

    added a dto for product

---
+++ common/src/main/java/com/huangbusiness/common/dto/ProductDto.java
@@ -0,0 +1,42 @@
+package com.huangbusiness.common.dto;
+
+import jakarta.validation.constraints.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+public class ProductDto {
+
+    public static interface UpdateProductAction {
+    }
+
+    @NotNull(groups = UpdateProductAction.class)
+    private Integer productId;
+
+    @NotBlank
+    @Size(min = 1, max = 128)
+    private String name;
+
+    @Size(min = 1, max = 512)
+    private String description;
+
+    @NotNull
+    @Digits(integer = 4, fraction = 2)
+    @DecimalMin("0.00")
+    private BigDecimal price;
+
+    @Digits(integer = 1, fraction = 2)
+    @DecimalMax("1")
+    @DecimalMin("0")
+    private BigDecimal discount;
+
+    @NotNull
+    @Min(1)
+    private Integer categoryId;
+
+    @NotNull(groups = UpdateProductAction.class)
+    private Boolean isAvailable;
+}
