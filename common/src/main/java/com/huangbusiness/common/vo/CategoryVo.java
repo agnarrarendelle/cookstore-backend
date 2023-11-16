d20dd7418808641dc9a0db7ef073c95537569b93
agnarrarendelle <danny20001102@gmail.com>
2023-11-16 16:25:40 +0800

    added a category vo

---
+++ common/src/main/java/com/huangbusiness/common/vo/CategoryVo.java
@@ -0,0 +1,13 @@
+package com.huangbusiness.common.vo;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class CategoryVo {
+    private Integer id;
+    private String name;
+}
