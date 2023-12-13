363b24b216e5ff71286373ea1d6c7ae6ca43f081
agnarrarendelle <danny20001102@gmail.com>
2023-12-13 12:54:21 +0800

    added a list of product vos to category vo

--- common/src/main/java/com/huangbusiness/common/vo/CategoryVo.java
+++ common/src/main/java/com/huangbusiness/common/vo/CategoryVo.java
@@ -4,10 +4,13 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 @Builder
 public class CategoryVo {
     private Integer id;
     private String name;
+    private List<ProductVo> products;
 }
