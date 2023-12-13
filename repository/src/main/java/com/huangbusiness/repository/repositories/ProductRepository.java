ac7f036e0401ef7e92f6342953a1e4acd23b7300
agnarrarendelle <danny20001102@gmail.com>
2023-12-13 12:55:32 +0800

    added a method to find products by category

--- repository/src/main/java/com/huangbusiness/repository/repositories/ProductRepository.java
+++ repository/src/main/java/com/huangbusiness/repository/repositories/ProductRepository.java
@@ -1,9 +1,13 @@
 package com.huangbusiness.repository.repositories;
 
+import com.huangbusiness.repository.entity.Category;
 import com.huangbusiness.repository.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
+    List<Product> findAllByCategory(Category category);
 }
