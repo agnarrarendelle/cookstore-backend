b95c97f1131647849d4798bbfbe2dd2756ca77b9
agnarrarendelle <danny20001102@gmail.com>
2023-12-19 19:10:32 +0800

    added a method to check if the admin user is connected

--- service/src/main/java/com/huangbusiness/service/handler/MyWebSocketHandler.java
+++ service/src/main/java/com/huangbusiness/service/handler/MyWebSocketHandler.java
@@ -1,19 +1,30 @@
-package com.huangbusiness.api.handler;
+package com.huangbusiness.service.handler;
 
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionConnectEvent;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Component
 public class MyWebSocketHandler {
+    final private Set<String> sessionIds = new HashSet<>();
     @EventListener
     private void handleSessionConnected(SessionConnectEvent event) {
+        String sessionId = event.getMessage().getHeaders().get("simpSessionId", String.class);
+        sessionIds.add(sessionId);
         System.out.println("WS CONNECTED");
     }
 
     @EventListener
     private void handleSessionDisconnect(SessionDisconnectEvent event) {
+        sessionIds.remove(event.getSessionId());
         System.out.println("WS DISCONNECTED");
+    }
+
+    public boolean isAdminConnected(){
+        return !sessionIds.isEmpty();
     }
 }
