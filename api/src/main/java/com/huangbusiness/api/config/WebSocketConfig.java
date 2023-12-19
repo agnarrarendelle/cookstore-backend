9b137b2d5c11f4502f61b913e8f1b66e5ecc64e0
agnarrarendelle <danny20001102@gmail.com>
2023-12-19 16:01:07 +0800

    added an interceptor to check if the websocket connect request is authenticated

--- api/src/main/java/com/huangbusiness/api/config/WebSocketConfig.java
+++ api/src/main/java/com/huangbusiness/api/config/WebSocketConfig.java
@@ -1,14 +1,35 @@
 package com.huangbusiness.api.config;
 
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.huangbusiness.common.exception.JwtAuthException;
+import com.huangbusiness.security.jwt.JwtUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpHeaders;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageHeaderAccessor;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+import java.util.Objects;
+
 @Configuration
 @EnableWebSocketMessageBroker
+@Order(Ordered.HIGHEST_PRECEDENCE + 99)
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Autowired
+    JwtUtils jwtUtils;
+
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.enableSimpleBroker("/topic", "/user");
@@ -20,5 +41,24 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws-endpoint").setAllowedOriginPatterns("*");
         registry.addEndpoint("/ws-endpoint").setAllowedOriginPatterns("*").withSockJS();
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(new ChannelInterceptor() {
+            @Override
+            public Message<?> preSend(Message<?> message, MessageChannel channel) {
+                StompHeaderAccessor accessor =
+                        MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
+                if (Objects.nonNull(accessor) && StompCommand.CONNECT.equals(accessor.getCommand())) {
+                    String authorization = accessor.getFirstNativeHeader(HttpHeaders.AUTHORIZATION);
+                    DecodedJWT jwt = jwtUtils.resolve(authorization);
+                    if (Objects.isNull(jwt)) {
+                        throw new JwtAuthException();
+                    }
+                }
+                return message;
+            }
+        });
     }
 }
