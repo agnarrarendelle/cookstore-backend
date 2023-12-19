4eae465c25fa03cc43af9502d1b6374e0df407b8
agnarrarendelle <danny20001102@gmail.com>
2023-12-19 16:02:42 +0800

    moved createOrder method elsewhere

--- api/src/main/java/com/huangbusiness/api/controller/OrderItemController.java
+++ api/src/main/java/com/huangbusiness/api/controller/OrderItemController.java
@@ -1,15 +1,10 @@
 package com.huangbusiness.api.controller;
 
-import com.huangbusiness.common.dto.OrderItemDto;
-import com.huangbusiness.common.result.Result;
 import com.huangbusiness.service.OrderItemService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 
 @RestController
 @RequestMapping("/order-item")
@@ -17,9 +12,5 @@ public class OrderItemController {
     @Autowired
     OrderItemService orderItemService;
 
-    @PostMapping
-    public Result<Void> createOrder(@RequestBody OrderItemDto orderItemDto){
-        orderItemService.createOrder(orderItemDto);
-        return Result.success();
-    }
+
 }
--- service/src/main/java/com/huangbusiness/service/OrderItemService.java
+++ service/src/main/java/com/huangbusiness/service/OrderItemService.java
@@ -1,10 +1,5 @@
 package com.huangbusiness.service;
 
-import com.huangbusiness.common.dto.OrderItemDto;
-
-import java.util.List;
-
 public interface OrderItemService {
 
-    void createOrder(OrderItemDto orderItemDto);
 }
