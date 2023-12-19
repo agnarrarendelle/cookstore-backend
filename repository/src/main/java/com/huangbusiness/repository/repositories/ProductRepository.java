0b4fbe11a0b408fb74a70a75ff82dc92e72b13ba
agnarrarendelle <danny20001102@gmail.com>
2023-12-19 16:01:31 +0800

    added a method to fetch products by status

--- repository/src/main/java/com/huangbusiness/repository/repositories/ProductRepository.java
+++ repository/src/main/java/com/huangbusiness/repository/repositories/ProductRepository.java
@@ -3,6 +3,8 @@ package com.huangbusiness.repository.repositories;
 import com.huangbusiness.repository.entity.Category;
 import com.huangbusiness.repository.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,4 +12,7 @@ import java.util.List;
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
     List<Product> findAllByCategory(Category category);
+
+    @Query("SELECT p FROM Product p WHERE p.id IN :productIds AND p.status = :status")
+    List<Product> findProductsByStatus(@Param("productIds") List<Integer> productIds, @Param("status") Product.ProductStatus status);
 }
